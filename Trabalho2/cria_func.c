d0bdc0e0fad3fd76e3943d8498925f6674bdbe6a
Davi Martignoni Barros <davimartignonibarros@gmail.com>
2024-11-12 08:40:13 -0300

    adicionando arquivos do trabalho 2

---
+++ Trabalho2/cria_func.c
@@ -0,0 +1,8 @@
+/* Davi Martignoni Barros 2311009 3WC */
+/* Luiz Felipe Neves Batista 2311024 3WC */
+
+#include "cria_func.h"
+
+void cria_func (void* f, DescParam params[], int n, unsigned char codigo[]) {
+    return;
+}
